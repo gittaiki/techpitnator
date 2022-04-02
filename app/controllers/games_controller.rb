7467ca601ba26d4e0619620eb825be7257b1a114
gittaiki <0215.taiki@gmail.com>
2022-04-02 10:17:11 +0900

    Add: ゲームのスタート処理を実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,10 @@ class GamesController < ApplicationController
 
   def new
   end
+
+  def create
+    # ゲームを始めるときはin_progress
+    Game.create!(status: 'in_progress')
+    render plain: "game started! Let's start next section."
+  end
 end
